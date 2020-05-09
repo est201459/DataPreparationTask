417b27cac08975174d6459a893e4e3d6295f772b
Elmer <Esteban>
2020-05-08 23:00:38 -0600

    VersionFInal
    VersionFinal

--- myproject/myproject/spiders/article.py
+++ myproject/myproject/spiders/article.py
@@ -1,40 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from myproject.items import articles, article
+from   myproject.items import articles, article
 
 class ArticleSpider(scrapy.Spider):
     name            = 'article'
-
+    page_number     = 2
+    total_request   = 0
     allowed_domains = ['catalog.data.gov']
-
-    start_urls      = ['https://catalog.data.gov/dataset']
-
+    start_urls      = ['https://catalog.data.gov/']
+    base_url        = ['https://catalog.data.gov/']
+    
     ##ROBOTSTXT_OBEY  = False
-
     def parse(self, response):
-
-        ##host = self.allowed_domains[0]
-
+        #host = self.allowed_domains[0]
+        
         for link in response.css(".dataset-heading > a"):
             link = f"{link.attrib.get('href')}"
-            title = link
-            yield response.follow(link,callback=self.parse_detail, meta={'link' : link,'title':title})
-
-
-
+            yield response.follow(link,callback=self.parse_detail, meta={'link' : link})
+ 
+        
     def parse_detail(self,response):
-
         items = articles()
         item = article()
-
         items["link"] = response.meta["link"]
-        item["title"] = response.css(".module-content > h1::text").extract()[0].strip() #response.meta["title"]
+        item["title"] = response.css(".module-content > h1::text").extract()[0].strip()
         item["paragraph"]=response.css(".notes >p::text").extract()[0].strip()
-        #item["paragraph"] = list()
-
-
-        #for text in response.css(".notes > p::text").extract():
-
-         #   item["paragraph"].append(text)
         items["body"] = item
         return items
